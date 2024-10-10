e5766deca606aa10503604bab2d625f879abf012
hafssa01 <hafssazaferr@gmail.com>
2024-10-10 19:13:42 +0100

    Annotations_concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' Description: type-annotated function aconcat that takes a string str1 and a string str2
+     as arguments and returns a concatenated string
+    Arguments:  str1: string
+                str2: string
+'''
+
+def concat (str1: str, str2: str) -> str:
+    '''Return concatenation of str1 and str2.'''
+    return str1 + str2
