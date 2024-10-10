135364ceba074cfbf0eb28e6968a3d9b09701b8e
hafssa01 <hafssazaferr@gmail.com>
2024-10-10 20:59:28 +0100

    sum_list_variables_annotaions

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -2,6 +2,7 @@
 ''' Description:  type-annotated function to_str that takes a float n as argument and returns the string representation of the float.
     Arguments:  n: float
 '''
+
 def to_str(n: float) -> str:
     '''Return string representation of n float'''
     return str(n)
