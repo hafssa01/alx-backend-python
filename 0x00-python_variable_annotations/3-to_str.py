c1f71f0fda725fbc9b2a3e41f9b67f188f831409
hafssa01 <hafssazaferr@gmail.com>
2024-10-10 22:05:04 +0100

    sum_mixed

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Description: type-annotated function to_str that takes a float n as argument and returns the string representation of the float.
+''' Description: type-annotated function to_str that takes a float n as argument and returns the string representation of the float.
     Arguments: n:float
 '''
 
