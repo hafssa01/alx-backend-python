fe56d91c17e7439a0d3fca6263ac17b50f15d3ab
hafssa01 <hafssazaferr@gmail.com>
2024-10-14 22:45:15 +0100

    basic_async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ This script is used to generate a random number between 1 and 100.
 '''
 import asyncio
 import random
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random amount of time between 0 and max_delay seconds."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
