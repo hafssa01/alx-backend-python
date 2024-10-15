d2c49a78beb8f08caf7e5b037b70592774228dbf
hafssa01 <hafssazaferr@gmail.com>
2024-10-15 21:59:38 +0100

    generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-'''
-This script is used to generate a random number between 1 and 10.
+'''Task 0's module.
 '''
 import asyncio
 import random
-from typing import AsyncGenerator
-async def async_generator() -> AsyncGenerator[float, None, None]:
-    '''This function generates a random number between 1 and 10.'''
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
