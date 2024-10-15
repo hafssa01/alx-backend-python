639ec7cd5d5e676dcc637a5bc10ae42cdab4702e
hafssa01 <hafssazaferr@gmail.com>
2024-10-15 21:57:48 +0100

    generator function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,9 @@ This script is used to generate a random number between 1 and 10.
 '''
 import asyncio
 import random
-
-async def async_generator():
+from typing import AsyncGenerator
+async def async_generator() -> AsyncGenerator[float, None, None]:
     '''This function generates a random number between 1 and 10.'''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(10)
+        yield random.random() * 10
